e806290ceb3adb080e335c9e22a1fcb34a05372d
Joo <dev.2ju2@gmail.com>
2025-07-19 10:01:17 +0900

    store 세팅 및 sortingorder 설정

---
+++ Assets/Background/SortingOrder.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SortingOrderByY : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        int order = -(int)(transform.position.y * 100);
+        sr.sortingOrder = order;
+
+        // 디버깅 로그 (한 번만 보고 싶으면 주석 처리해도 돼)
+        Debug.Log($"{gameObject.name} - Y: {transform.position.y:F2} → Order: {order}");
+    }
+}
